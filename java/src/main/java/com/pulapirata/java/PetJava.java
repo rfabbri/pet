c47c9a5f6d860cf445d0a18ef00b2f96e704535d
Ricardo Fabbri <rfabbri@gmail.com>
2013-01-03 00:11:19 -0200

    window resize for java working

--- java/src/main/java/com/pulapirata/java/PetJava.java
+++ java/src/main/java/com/pulapirata/java/PetJava.java
@@ -8,7 +8,14 @@ import com.pulapirata.core.Pet;
 public class PetJava {
 
   public static void main(String[] args) {
-    JavaPlatform platform = JavaPlatform.register();
+    // JavaPlatform platform = JavaPlatform.register();
+
+    JavaPlatform.Config conf = new JavaPlatform.Config();
+    conf.width = 480;
+    conf.height = 440;
+
+    JavaPlatform platform = JavaPlatform.register(conf);
+
     platform.assets().setPathPrefix("com/pulapirata/resources");
     PlayN.run(new Pet());
   }
