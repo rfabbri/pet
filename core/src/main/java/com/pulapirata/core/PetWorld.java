efa3e744cfdaf77f904ce462b787ef44c3c530b0
Ricardo Fabbri <rfabbri@lems.brown.edu>
2014-09-28 01:14:30 -0300

    basic velocity and animator stub for pet

--- core/src/main/java/com/pulapirata/core/PetWorld.java
+++ core/src/main/java/com/pulapirata/core/PetWorld.java
@@ -11,7 +11,9 @@ class PetWorld extends World {
     /** Misc variables */
 
     public final GroupLayer layer_;
-    public final Signal<Key> keyReloadGameFile = Signal.create();
+    public final Signal<Key> keyDown = Signal.create();
+    public final Signal<Key> keyUp = Signal.create();
+
 
     /*-------------------------------------------------------------------------------*/
     /** Types of entities */
@@ -29,12 +31,17 @@ class PetWorld extends World {
      */
     public final Component.IMask type = new Component.IMask(this);
     public final Component.XY pos = new Component.XY(this);
+    public final Component.XY vel = new Component.XY(this); // pixels/ms
+
     public final Component.IScalar expires = new Component.IScalar(this);
     private final Randoms _rando = Randoms.with(new Random());
 
     /*-------------------------------------------------------------------------------*/
     /** Time data */
     private int beat_ = 0; // total number of updates so far
+
+    /*-------------------------------------------------------------------------------*/
+    /** Misc methods */
 
     @Override public void update (int delta) {
         beat_ += delta;
@@ -62,4 +69,54 @@ class PetWorld extends World {
         default: return "unknown:" + type.get(id);
       }
     }
+
+    /*-------------------------------------------------------------------------------*/
+    /** Systems */
+
+
+    /** Use keys to control pet. Like in minigames. Pet should automatically move
+     * and do something fun if no control is pressed. NOOP if touchscreen
+     * or gamepad are available.
+     */
+    public final System walkControls = new System(this, 1) {
+        /* ctor */ {
+            keyDown.connect(new Slot<Key>() {
+                @Override public void onEmit (Key key) {
+                    switch (key) {
+                      // TODO colocar estado walk_velocity_ na classe pet?
+                      case LEFT:  vel_.x = -WALK_VELOCITY;  vel_.y = 0;  break;
+                      case RIGHT: vel_.x  =  WALK_VELOCITY;  vel_.y = 0;  break;
+                      case UP:    vel_.x  =  0;  vel_.y =  WALK_VELOCITY;  break;
+                      case DOWN:  vel_.x  =  0;  vel_.y = -WALK_VELOCITY;  break;
+                      case SPACE: System.out.println("Key SPACE pressed: u mean jump?"); break;
+                      case C: System.out.println("Key C pressed: u mean taka dump?"); break;
+                    default: break;
+                    }
+                }
+            });
+            keyUp.connect(new Slot<Key>() {
+                @Override public void onEmit (Key key) {
+                    switch (key) {
+                      case LEFT:  vel_.x = 0; vel_.y = 0; break;
+                      case RIGHT: vel_.x = 0; vel_.y = 0;  break;
+                      case UP:    vel_.x = 0; vel_.y = 0; break;
+                      case DOWN:  vel_.x = 0; vel_.y = 0;  break;
+                    default: break;
+                    }
+                }
+            });
+        }
+
+        @Override protected void wasAdded (Entity entity) {
+            super.wasAdded(entity);
+            _ship = entity;
+        }
+
+        @Override protected boolean isInterested (Entity entity) {
+            return type.get(entity.id) == PET;
+        }
+
+        protected Vector vel_ = new Vector();
+    };
+
 }
