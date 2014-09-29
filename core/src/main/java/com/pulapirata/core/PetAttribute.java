bbc0d677d95f032da0a60d1bf995df6debd10e93
Ricardo Fabbri <rfabbri@lems.brown.edu>
2014-09-29 00:35:07 -0300

    attribute class now reactive

--- core/src/main/java/com/pulapirata/core/PetAttribute.java
+++ core/src/main/java/com/pulapirata/core/PetAttribute.java
@@ -1,19 +1,30 @@
 package com.pulapirata.core;
 import java.util.*;
+
 import playn.core.PlayN;
 
-public class PetAttribute {
+import react.IntValue;
+
+/**
+ * A reactive pet attribute. It can be wired to listeners to do all sorts of
+ * game logic stuff.
+ */
+public class PetAttribute extends IntValue {
 
   public PetAttribute(String name, int startVal, int min, int max, int passive, int passiveBeats) {
+    super(startVal);
     set(name, startVal, min, max, passive, passiveBeats);
+    assert inv();
   }
 
-  // start with easily identifiable dummy default values
+  /**
+   * start with easily identifiable dummy default values
+   */
   public PetAttribute(String name) {
     set(name, -6969, -696969, 696969, 6969, 9696);
   }
 
-  public int val() { return val_; }
+  public int val() { return get(); }
   public int min() { return min_; }
   public int max() { return max_; }
   public int passive() { return passive_; }
@@ -22,35 +33,47 @@ public class PetAttribute {
   public String name() { return name_; }
 
   public void set(int v) {
-    val_ = v;
-    assertConsistency();
+    updateInt(v);
+    assert inv();
   }
 
   public void set(String name, int startVal, int min, int max, int passive, int passiveBeats) {
     name_ = name;
-    val_ = startVal;
     min_ = min;
     max_ = max;
     passive_ = passive;
     passiveBeats_ = passiveBeats;
-    assertConsistency();
+    set(startVal);
   }
 
-  // TODO: substitute by assertion
-  void assertConsistency() {
+  /**
+   * Sets everything except start value
+   */
+  protected void set(String name, int min, int max, int passive, int passiveBeats) {
+    name_ = name;
+    min_ = min;
+    max_ = max;
+    passive_ = passive;
+    passiveBeats_ = passiveBeats;
+  }
+
+  /**
+   * Class invariant that min, max, val, etc are consistent.
+   */
+  void inv() {
     if (min_ > val_ || val_ > max_) {
       System.out.println("Inconsistency in attribute " + name_);
       PlayN.log().error("Inconsistency in attribute " + name_);
     }
   }
 
-  public void sum(int v) { val_ += v; assertConsistency(); }
-  public void sub(int v) { val_ -= v; assertConsistency(); }
-  public void sumPassive() { sum(passive()); assertConsistency(); }
-  public void subPassive() { sub(passive()); assertConsistency(); }
-  public void setMin(int v) { min_ = v; assertConsistency(); } // TODO do some checking
-  public void setMax(int v) { max_ = v; assertConsistency(); }
-  public void setPassive(int p) { passive_ = p; assertConsistency(); }
+  public void sum(int v) { incrementClamp(v, max_); }
+  public void sub(int v) { decrementClamp(v, min_); }
+  public void sumPassive() { sum(passive()); }
+  public void subPassive() { sub(passive()); }
+  public void setMin(int v) { min_ = v; assert inv(); }
+  public void setMax(int v) { max_ = v; assert inv(); }
+  public void setPassive(int p) { passive_ = p; assert inv(); }
   public void setPassiveBeats(int b) { passiveBeats_ = (b>1)? b:1; }
   // TODO updatePassivo();
 
@@ -61,7 +84,6 @@ public class PetAttribute {
   }
 
   protected String name_;
-  protected int val_;
   protected int min_;
   protected int max_;
   protected int passive_;
