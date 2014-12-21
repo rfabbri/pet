9644bc877c0bc4ac7e3b21523dc359efa216fc05
Ricardo Fabbri <rfabbri@lems.brown.edu>
2014-12-21 13:49:05 -0300

    PetSpriter reflects ageStage

--- core/src/main/java/com/pulapirata/core/sprites/PetSpriter.java
+++ core/src/main/java/com/pulapirata/core/sprites/PetSpriter.java
@@ -13,6 +13,7 @@ import static playn.core.PlayN.log;
 import com.pulapirata.core.PetAttributes;
 import com.pulapirata.core.PetAttributes.VisibleCondition;
 import static com.pulapirata.core.PetAttributes.VisibleCondition.*;
+import com.pulapirata.core.PetAttributes.AgeStage;
 import static com.pulapirata.core.utils.Puts.*;
 
 /**
@@ -25,7 +26,9 @@ public class PetSpriter extends CompositeSpriter {
 //    public static String IMAGE = "pet/sprites/atlas.png";
 //    public static String JSON = "pet/sprites/atlas.json";
 
-    private final ArrayList<String> images =
+
+
+    private final ArrayList<String> imagesBebe =
         new ArrayList<String>(Arrays.asList(
                 "pet/sprites/Pingo/Bebe/pingo_bebe_bebado_v2.png",
                 "pet/sprites/Pingo/Bebe/pingo_bebe_chorando_v2.png",
@@ -42,7 +45,7 @@ public class PetSpriter extends CompositeSpriter {
                 "pet/sprites/overlays/questionmark.png"
         ));
 
-    private final ArrayList<String> jsons =
+    private final ArrayList<String> jsonsBebe =
         new ArrayList<String>(Arrays.asList(
                 "pet/sprites/Pingo/Bebe/pingo_bebe_bebado_v2.json",
                 "pet/sprites/Pingo/Bebe/pingo_bebe_chorando_v2.json",
@@ -59,7 +62,7 @@ public class PetSpriter extends CompositeSpriter {
                 "pet/sprites/overlays/questionmark.json"
         ));
 
-    private final ArrayList<VisibleCondition> vc =
+    private final ArrayList<VisibleCondition> vcBebe =
         new ArrayList<VisibleCondition>(Arrays.asList(
                 BEBADO,
                 CHORANDO,
@@ -75,7 +78,6 @@ public class PetSpriter extends CompositeSpriter {
                 VARRENDO,
                 UNDETERMINED
         ));
-
     // Atualmente nao tem sprite antigo para estes:
     //        IRRITADO,
     //        BRAVO,
@@ -92,61 +94,208 @@ public class PetSpriter extends CompositeSpriter {
     //        NORMAL_COM_VOMITO,
     //        BEBADO_VOMITANDO,
 
+    private final ArrayList<String> imagesCrianca =
+        new ArrayList<String>(Arrays.asList(
+                "pet/sprites/Pingo/Bebe/pingo_bebe_bebado_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_chorando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_coma_alcoolico_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_comendo_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_dormindo_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_doente_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_morto_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_piscando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_pulando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_triste_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_vomitando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_varrendo_v2.png",
+                "pet/sprites/overlays/questionmark.png"
+        ));
+
+    private final ArrayList<String> jsonsCrianca =
+        new ArrayList<String>(Arrays.asList(
+                "pet/sprites/Pingo/Bebe/pingo_bebe_bebado_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_chorando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_coma_alcoolico_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_comendo_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_dormindo_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_doente_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_morto_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_piscando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_pulando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_triste_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_vomitando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_varrendo_v2.json",
+                "pet/sprites/overlays/questionmark.json"
+        ));
+
+    private final ArrayList<VisibleCondition> vcCrianca =
+        new ArrayList<VisibleCondition>(Arrays.asList(
+                BEBADO,
+                CHORANDO,
+                COMA_ALCOOLICO,
+                COMENDO,
+                DORMINDO,
+                DOENTE,
+                MORTO,
+                NORMAL,
+                PULANDO,
+                TRISTE,
+                VOMITANDO,
+                VARRENDO,
+                UNDETERMINED
+        ));
+
+    private final ArrayList<String> imagesAdolescente =
+        new ArrayList<String>(Arrays.asList(
+                "pet/sprites/Pingo/Bebe/pingo_bebe_bebado_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_chorando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_coma_alcoolico_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_comendo_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_dormindo_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_doente_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_morto_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_piscando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_pulando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_triste_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_vomitando_v2.png",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_varrendo_v2.png",
+                "pet/sprites/overlays/questionmark.png"
+        ));
+
+    private final ArrayList<String> jsonsAdolescente =
+        new ArrayList<String>(Arrays.asList(
+                "pet/sprites/Pingo/Bebe/pingo_bebe_bebado_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_chorando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_coma_alcoolico_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_comendo_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_dormindo_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_doente_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_morto_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_piscando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_pulando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_triste_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_vomitando_v2.json",
+                "pet/sprites/Pingo/Bebe/pingo_bebe_varrendo_v2.json",
+                "pet/sprites/overlays/questionmark.json"
+        ));
+
+    private final ArrayList<VisibleCondition> vcAdolescente =
+        new ArrayList<VisibleCondition>(Arrays.asList(
+                BEBADO,
+                CHORANDO,
+                COMA_ALCOOLICO,
+                COMENDO,
+                DORMINDO,
+                DOENTE,
+                MORTO,
+                NORMAL,
+                PULANDO,
+                TRISTE,
+                VOMITANDO,
+                VARRENDO,
+                UNDETERMINED
+        ));
+
+
+    private final ArrayList< ArrayList<String> > images =
+        new ArrayList< ArrayList<String> >(Arrays.asList(
+                imagesBebe,
+                imagesCrianca,
+                imagesAdolescente
+        ));
+
+    private final ArrayList< ArrayList<String> > jsons =
+        new ArrayList< ArrayList<String> >(Arrays.asList(
+                jsonsBebe,
+                jsonsCrianca,
+                jsonsAdolescente
+        ));
+
+    private final ArrayList< ArrayList<VisibleCondition > > vc =
+        new ArrayList< ArrayList<VisibleCondition> >(Arrays.asList(
+                vcBebe,
+                vcCrianca,
+                vcAdolescente
+        ));
+
+    AgeStage age_ = AgeStage.BEBE;
+
+    int totalNumSprites_ = -1;
+
+    public int totalNumSprites() {
+        if (totalNumSprites_ != -1)
+            return totalNumSprites_;
+        int t = 0;
+        for (int a = 0; a < jsons.size(); ++a)
+            t += jsons.get(a).size();
+        return (totalNumSprites_ = t);
+    }
+
     @Override
     public boolean hasLoaded() {
-        return numLoaded_ == jsons.size();
+        return numLoaded_ == totalNumSprites();
     }
 
     // all member animations(sprites) should have same atlas as source,
     // as built in PetSpriteLoader.java, and also the same layer
-    private EnumMap<VisibleCondition, Sprite> animMap_ = new EnumMap<VisibleCondition, Sprite> (VisibleCondition.class);
+
+
+    // animMap_.get(age_).get(NORMAL)
+    private EnumMap<AgeStage, EnumMap<VisibleCondition, Sprite> > animMap_ =
+        new EnumMap<AgeStage, EnumMap<VisibleCondition, Sprite> > (AgeStage.class);
     private VisibleCondition currentVisibleCondition_;   // the current enum of the sprite animation
 
     public PetSpriter() {
-        for (int i = 0; i < jsons.size(); i++) {
-            String spriteFnames = images.get(i);
-            String jsonFnames   = jsons.get(i);
-            printd("[petspriter] Loading sprite file: " + spriteFnames + jsonFnames);
-            Sprite s = SpriteLoader.getSprite(spriteFnames, jsonFnames);
-            //System.out.println("sprite true? : " + sprite == null + "i : " + i + vc.size());
-            animMap_.put(vc.get(i), s);
+        assert images.size() == jsons.size() && jsons.size() == vc.size();
 
-            // Add a callback for when the image loads.
-            // This is necessary because we can't use the width/height (to center the
-            // image) until after the image has been loaded
-            s.addCallback(new Callback<Sprite>() {
-                @Override
-                public void onSuccess(Sprite sprite) {
-                    sprite.setSprite(0);
-                    sprite.layer().setOrigin(0, 0);
-                    sprite.layer().setTranslation(0, 0);
-//                    sprite.layer().setScale(4);
-                    if (sprite == animMap_.get(NORMAL))   // start with normal by default.
-                        set(NORMAL);
-                    else
-                        sprite.layer().setVisible(false);
-                    dprint("[petspriter] added, visible: " + sprite.layer().visible() + " full layer: " + animLayer_.visible());
-                    animLayer_.add(sprite.layer());
-                    numLoaded_++;
+        for (int a = 0; a < jsons.size(); a++) { // for each age stage
+            EnumMap<VisibleCondition, Sprite> animMapForAge = new EnumMap<VisibleCondition, Sprite> (VisibleCondition.class);
+            animMap_.put(AgeStage.values()[a], animMapForAge);
+            for (int i = 0; i < jsons.get(a).size(); i++) {
+                String spriteFnames = images.get(a).get(i);
+                String jsonFnames   = jsons.get(a).get(i);
+                printd("[petspriter] Loading sprite file: " + spriteFnames + jsonFnames);
+                Sprite s = SpriteLoader.getSprite(spriteFnames, jsonFnames);
+                //System.out.println("sprite true? : " + sprite == null + "i : " + i + vc.size());
+                animMap_.get(AgeStage.values()[a]).put(vc.get(a).get(i), s);
+
+                // Add a callback for when the image loads.
+                // This is necessary because we can't use the width/height (to center the
+                // image) until after the image has been loaded
+                s.addCallback(new Callback<Sprite>() {
+                    @Override
+                    public void onSuccess(Sprite sprite) {
+                        sprite.setSprite(0);
+                        sprite.layer().setOrigin(0, 0);
+                        sprite.layer().setTranslation(0, 0);
+    //                    sprite.layer().setScale(4);
+                        if (sprite == animMap_.get(age_).get(NORMAL))   // start with normal by default.
+                            set(NORMAL);
+                        else
+                            sprite.layer().setVisible(false);
+                        dprint("[petspriter] added, visible: " + sprite.layer().visible() + " full layer: " + animLayer_.visible());
+                        animLayer_.add(sprite.layer());
+                        numLoaded_++;
+                    }
+
+                    @Override
+                    public void onFailure(Throwable err) {
+                        log().error("Error loading image!", err);
+                    }
+                });
+            }
+
+            // Error check of internal structures - ifndef NDEBUG
+            int n = VisibleCondition.values().length;
+            boolean[] hasState = new boolean[n];
+            for (int i = 0; i < vc.size(); ++i) {
+                hasState[vc.get(a).get(i).ordinal()] = true;
+            }
+            for (int i = 0; i < n; ++i) {
+                if (!hasState[i]) {
+                    dprint("Warning: sprite file not specified for state " + VisibleCondition.values()[i] + " age " + age_);
+                    dprint("         make sure this is rendered some other way");
                 }
-
-                @Override
-                public void onFailure(Throwable err) {
-                    log().error("Error loading image!", err);
-                }
-            });
-        }
-
-        // Error check of internal structures - ifndef NDEBUG
-        int n = VisibleCondition.values().length;
-        boolean[] hasState = new boolean[n];
-        for (int i = 0; i < vc.size(); ++i) {
-            hasState[vc.get(i).ordinal()] = true;
-        }
-        for (int i = 0; i < n; ++i) {
-            if (!hasState[i]) {
-                dprint("Warning: sprite file not specified for state " + VisibleCondition.values()[i]);
-                dprint("         make sure this is rendered some other way");
             }
         }
     }
@@ -155,9 +304,9 @@ public class PetSpriter extends CompositeSpriter {
      * Sets animation based on pet's current visible condition
      */
     public void set(VisibleCondition s) {
-        Sprite newSprite = animMap_.get(s);
+        Sprite newSprite = animMap_.get(age_).get(s);
         if (newSprite == null) {
-            pprint("[petspriter.set] Warning: no direct anim for requested visibleCondition " + s);
+            pprint("[petspriter.set] Warning: no direct anim for requested visibleCondition " + s + " age " + age_);
             // Handle a different way of animating this visible
             // condition (composite anims or synthetic or flump)
             //
@@ -214,11 +363,29 @@ public class PetSpriter extends CompositeSpriter {
         set(VisibleCondition.values()[i]);
     }
 
+    public void setClass(int i) {
+        age_ = AgeStage.values()[i];
+    }
+
     @Override
     public void update(int delta) {
         super.update(delta);
         if (hasLoaded())
             dprint("[petSpriter] currentVisibleCondition: " + currentVisibleCondition_);
+    }
+
+    /**
+     * Returns a slot which can be used to wire the class (eg, AgeStage) of the
+     * current sprite animation to the emissions of a {@link Signal} or another value.
+     * There is an animation set in a composite spriter. The PetSpriter has a
+     * class which changes the animation set. This can be ageState, or style, etc.
+     */
+    public Slot<Integer> classSlot() {
+        return new Slot<Integer>() {
+            @Override public void onEmit (Integer value) {
+                setClass(value);
+            }
+        };
     }
 
 //    /**
