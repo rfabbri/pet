6b64b7c4551ca1d98dcebbbe007ac039e8f2462a
Ricardo Fabbri <rfabbri@lems.brown.edu>
2014-12-26 08:49:09 -0300

    GPL-ized source files

--- core/src/main/java/com/pulapirata/core/Action.java
+++ core/src/main/java/com/pulapirata/core/Action.java
@@ -1,3 +1,28 @@
+/**
+ * Pet - a comic pet simulator game
+ * Copyright (C) 2013-2015 Ricardo Fabbri and Edson "Presto" Correa
+ *
+ * This program is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version. A different license may be requested
+ * to the copyright holders.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * NOTE: this file may contain code derived from the PlayN, Tripleplay, and
+ * React libraries, all (C) The PlayN Authors or Three Rings Design, Inc.  The
+ * original PlayN is released under the Apache, Version 2.0 License, and
+ * TriplePlay and React have their licenses listed in COPYING-OOO. PlayN and
+ * Three Rings are NOT responsible for the changes found herein, but are
+ * thankfully acknowledged.
+ */
 package com.pulapirata.core;
 import com.pulapirata.core.PetAttributes.ActionState;
 import com.pulapirata.core.Pet;
--- core/src/main/java/com/pulapirata/core/sprites/SpriteImage.java
+++ core/src/main/java/com/pulapirata/core/sprites/SpriteImage.java
@@ -1,4 +1,29 @@
 /**
+ * Pet - a comic pet simulator game
+ * Copyright (C) 2013-2015 Ricardo Fabbri and Edson "Presto" Correa
+ *
+ * This program is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version. A different license may be requested
+ * to the copyright holders.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
+ *
+ * NOTE: this file may contain code derived from the PlayN, Tripleplay, and
+ * React libraries, all (C) The PlayN Authors or Three Rings Design, Inc.  The
+ * original PlayN is released under the Apache, Version 2.0 License, and
+ * TriplePlay and React have their licenses listed in COPYING-OOO. PlayN and
+ * Three Rings are NOT responsible for the changes found herein, but are
+ * thankfully acknowledged.
+ */
+/**
  * Copyright 2011 The PlayN Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
